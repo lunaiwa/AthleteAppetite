d81b5d3f466f974dc4fc13d8793348f0880fb5dd
e-shen2022 <aimashen.2017@gmail.com>
2023-09-07 09:21:48 -0700

    emma broke things

--- logic.py
+++ logic.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
-
+# Import libraries for Flask framework, HTML template, and handling HTTP requests 
+from flask import Flask 
+from flask import render_template, request
 import requests
+
+# Create Flask application instance 
 app = Flask(__name__)
 
+# Define API URL and headers 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 
 headers = {
@@ -10,24 +14,32 @@ headers = {
   'x-rapidapi-key': "d2f7c75442mshde016465b174ed9p10a7b5jsn1acedf7fc107",
   }
 
+# Define API endpoints to retrieve different stuff 
 random_joke = "food/jokes/random"
 find = "recipes/findByIngredients"
 randomFind = "recipes/random"
 
+# Route handler 
 @app.route('/')
 def search_page():
-  joke_response = str(requests.request("GET", url + random_joke, headers=headers).json()['text'])
-  return render_template('search.html', joke=joke_response)
 
-if __name__ == '__main__':
-  app.run()
-  
+# Fetch random food-related joke & render on search page  
+# Change: removed str() conversion and work with response object directly 
+  joke_response = requests.request("GET", url + random_joke, headers=headers)
+  joke_data = joke_response.json()
+  joke_text = joke_data.get('text', 'No joke available')  # Handle the case where 'text' is not in the response
+  print(joke_text)
+  return render_template('search.html', joke=joke_text)
+
 @app.route('/recipes')
 def get_recipes():
+  # Check for ingredients 
   if (str(request.args['ingredients']).strip() != ""):
       # If there is a list of ingridients -> list
       querystring = {"number":"5","ranking":"1","ignorePantry":"false","ingredients":request.args['ingredients']}
+      # Send HTTP GET request to API using 'find' endpoint, converted to JSON format
       response = requests.request("GET", url + find, headers=headers, params=querystring).json()
+      # Render on recipes.html template
       return render_template('recipes.html', recipes=response)
   else:
       # Random recipes
@@ -36,26 +48,13 @@ def get_recipes():
       print(response)
       return render_template('recipes.html', recipes=response['recipes'])
 
-@app.route('/recipe')
-def get_recipe():
-  recipe_id = request.args['id']
-  recipe_info_endpoint = "recipes/{0}/information".format(recipe_id)
-  ingedientsWidget = "recipes/{0}/ingredientWidget".format(recipe_id)
-  equipmentWidget = "recipes/{0}/equipmentWidget".format(recipe_id)
+# Run Flask application 
+if __name__ == '__main__':
+  # Flask debug mode 
+  app.debug = True
+  app.run()
 
-  recipe_info = requests.request("GET", url + recipe_info_endpoint, headers=headers).json()
-    
-  recipe_headers = {
-      'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      'x-rapidapi-key': "d2f7c75442mshde016465b174ed9p10a7b5jsn1acedf7fc107",
-      'accept': "text/html"
-  }
-  querystring = {"defaultCss":"true", "showBacklink":"false"}
-
-  recipe_info['ingredientsWidget'] = requests.request("GET", url + ingedientsWidget, headers=recipe_headers, params=querystring).text
-  recipe_info['equipmentWidget'] = requests.request("GET", url + equipmentWidget, headers=recipe_headers, params=querystring).text
-    
-  return render_template('recipe.html', recipe=recipe_info)
+  
 
 
   
